d4c9e66a0513e456df0f7d0387e86acf48f5bbd5
TingSyuanWang <vincent@fishboneapps.com>
2023-02-09 19:15:05 -0500

    feat: change UI element programmatically
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 }
 
